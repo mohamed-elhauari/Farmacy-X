0a9d2b43fe804a5d2361a5528be611ef4184d032
elhauari-mohamed <elhauari.imohamed@etu.uae.ac.ma>
2025-05-26 02:33:14 +0100

    medicines index & show

---
+++ resources/views/customer/medicines/show.blade.php
@@ -0,0 +1,54 @@
+<x-app-customer-layout>
+
+
+
+    <div class="min-h-[calc(100vh-4rem)] flex flex-col md:flex-row items-center justify-center py-16 px-4 lg:px-12 xl:px-24 md:py-32 bg-white dark:bg-gray-800 md:gap-6 transition-all duration-300">
+
+        <div class="grid grid-cols-2 gap-2 max-w-lg">
+            <div>
+                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+            </div>
+            <div>
+                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
+            </div>
+        </div>
+
+        <div class="w-full sm:w-[512px]">
+
+            <h1 class="mb-4 mt-8 text-start text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">Doliprane,<span class="text-green-600 dark:text-green-500"> (Paracétamol - SANOFI)</span></h1>
+
+            <ul class="max-w-md space-y-1 text-gray-500 list-none list-inside dark:text-gray-400">
+                <li>
+                    <b class="text-gray-600 dark:text-gray-300">Type :</b> Comprimé 1000mg
+                </li>
+                <li>
+                    <b class="text-gray-600 dark:text-gray-300">Catégorie :</b> Antidouleur et anti-inflammatoire
+                </li>
+                <li>
+                    <b class="text-gray-600 dark:text-gray-300">Statut :</b> Disponible sans ordonnance
+                </li>
+                <li>
+                    <b class="text-gray-600 dark:text-gray-300">Prix Public (PPV) :</b> 15,50 MAD
+                </li>
+            </ul>
+
+            <button type="button" class="flex mt-5 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                <svg class="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4" />
+                </svg>
+                AJOUTER AU PANIER
+            </button>
+
+
+            
+        </div>
+
+    </div>
+
+</x-app-customer-layout>
