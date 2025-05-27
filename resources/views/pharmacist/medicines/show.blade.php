b0f58f92573f0876a85dc7ed115397262073ab91
elhauari-mohamed <elhauari.imohamed@etu.uae.ac.ma>
2025-05-27 06:31:11 +0100

    Add medicines csv

--- resources/views/pharmacist/medicines/show.blade.php
+++ resources/views/pharmacist/medicines/show.blade.php
@@ -49,7 +49,17 @@
 
     </div>
 
-    <div class=" pb-12 pt-6 px-4 lg:px-12 xl:px-24 ">
+
+
+    <div class="pb-12 pt-6 px-4 lg:px-12 xl:px-24">
+
+        <button type="button" class="w-full max-w-64 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800  text-center inline-flex items-center mt-3">
+            <svg class="mr-2 w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5"/>
+            </svg>
+            <a href="{{ route('pharmacist.medicines.add-infos') }}">Ajouter médicament</a>
+        </button>
+
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
